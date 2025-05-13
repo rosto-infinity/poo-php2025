4c872755641750a8218df76c2d35da53cf4e5a55
ROSTAND LELE <rostoinfocus@gmail.com>
2025-05-13 20:22:37 +0100

    -Affiche les informations de l'employé 1

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $employer2 = new Employer("Tagne", "Boris", 20); // Employé 2
 $employer3 = new Employer("Myra", "Eva", 15);   // Employé 3
 
 // -Appel de la méthode presentation() pour chaque employé
-$employer1->presentation(); // Affiche les informations de l'employé 1
+$employer1->presentation(); // -Affiche les informations de l'employé 1
 $employer2->presentation(); // Affiche les informations de l'employé 2
 $employer3->presentation(); // Affiche les informations de l'employé 3
 
